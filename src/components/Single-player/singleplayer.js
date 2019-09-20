7e5344c315acd356162a24340e205c66c30649eb
GuitarGuy244 <rgsl.salazar@gmail.com>
2019-09-19 18:58:41 -0700

    658

---
+++ src/components/Single-player/singleplayer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import WelcomeBanner from "./welcome-banner";
+import PlayButton from "./play-button";
+
+export default function SinglePlayer() {
+  return (
+    <div>
+      <WelcomeBanner />
+      <PlayButton />
+    </div>
+  );
+}
