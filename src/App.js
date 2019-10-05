7bc3dd701c1078aa44e2680ad4eec56c5adb9dbb
GuitarGuy244 <rgsl.salazar@gmail.com>
2019-10-05 09:32:50 -0700

    My commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import MiniDrawer from "./components/drawer";
+// import MiniDrawer from "./components/drawer";
+import Drawer from "./components/drawer"
 import Login from "./components/Login/login";
 import Register from "./components/register/register";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
@@ -9,7 +10,7 @@ function App() {
   return (
     <Router>
       <Route path="/register" exact component={Register} />
-      <Route path="/dashboard" exact component={MiniDrawer} />
+      <Route path="/dashboard" exact component={Drawer} />
       <Route path="/" exact component={Login} />
       <Route path="/game" exact component={Game} />
     </Router>
